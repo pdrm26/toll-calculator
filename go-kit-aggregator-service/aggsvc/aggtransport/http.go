11e70d8470c015f5fcdb6b381ceb76432563f04e
pedram <peeeedii@gmail.com>
2025-07-31 00:30:33 +0330

    feat: add http client

--- go-kit-aggregator-service/aggsvc/aggtransport/http.go
+++ go-kit-aggregator-service/aggsvc/aggtransport/http.go
@@ -1,16 +1,23 @@
 package aggtransport
 
 import (
+	"bytes"
 	"context"
 	"encoding/json"
+	"errors"
 	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+
 	"github.com/go-kit/kit/endpoint"
 	"github.com/go-kit/kit/transport"
 	httptransport "github.com/go-kit/kit/transport/http"
 	"github.com/go-kit/log"
-	"net/http"
 
 	"github.com/pdrm26/toll-calculator/go-kit-aggregator-service/aggsvc/aggendpoint"
+	"github.com/pdrm26/toll-calculator/go-kit-aggregator-service/aggsvc/aggservice"
 )
 
 func NewHTTPHandler(endpoints aggendpoint.Set, logger log.Logger) http.Handler {
@@ -32,6 +39,44 @@ func NewHTTPHandler(endpoints aggendpoint.Set, logger log.Logger) http.Handler {
 		options...,
 	))
 	return m
+}
+
+func NewHTTPClient(instance string, logger log.Logger) (aggservice.Service, error) {
+	if !strings.HasPrefix(instance, "http") {
+		instance = "http://" + instance
+	}
+	u, err := url.Parse(instance)
+	if err != nil {
+		return nil, err
+	}
+
+	var options []httptransport.ClientOption
+	var aggregateEndpoint endpoint.Endpoint
+	{
+		aggregateEndpoint = httptransport.NewClient(
+			"POST",
+			copyURL(u, "/aggregate"),
+			encodeHTTPGenericRequest,
+			decodeHTTPAggregateResponse,
+			options...,
+		).Endpoint()
+	}
+
+	var invoiceEndpoint endpoint.Endpoint
+	{
+		invoiceEndpoint = httptransport.NewClient(
+			"GET",
+			copyURL(u, "/invoice"),
+			encodeHTTPGenericRequest,
+			decodeHTTPInvoiceResponse,
+			options...,
+		).Endpoint()
+	}
+
+	return aggendpoint.Set{
+		AggregateEndpoint: aggregateEndpoint,
+		InvoiceEndpoint:   invoiceEndpoint,
+	}, nil
 }
 
 func errorEncoder(_ context.Context, err error, w http.ResponseWriter) {
@@ -58,4 +103,38 @@ func encodeHTTPGenericResponse(ctx context.Context, w http.ResponseWriter, respo
 
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	return json.NewEncoder(w).Encode(response)
+}
+
+func copyURL(base *url.URL, path string) *url.URL {
+	next := *base
+	next.Path = path
+	return &next
+}
+
+func encodeHTTPGenericRequest(_ context.Context, r *http.Request, request interface{}) error {
+	var buf bytes.Buffer
+	if err := json.NewEncoder(&buf).Encode(request); err != nil {
+		return err
+	}
+
+	r.Body = io.NopCloser(&buf)
+	return nil
+}
+
+func decodeHTTPAggregateResponse(_ context.Context, r *http.Response) (interface{}, error) {
+	if r.StatusCode != http.StatusOK {
+		return nil, errors.New(r.Status)
+	}
+	var resp aggendpoint.AggregateResponse
+	err := json.NewDecoder(r.Body).Decode(&resp)
+	return resp, err
+}
+
+func decodeHTTPInvoiceResponse(_ context.Context, r *http.Response) (interface{}, error) {
+	if r.StatusCode != http.StatusOK {
+		return nil, errors.New(r.Status)
+	}
+	var resp aggendpoint.InvoiceResponse
+	err := json.NewDecoder(r.Body).Decode(&resp)
+	return resp, err
 }
