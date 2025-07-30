e611c721c5c88d98c1af19681e312fce9566151a
pedram <peeeedii@gmail.com>
2025-07-30 23:34:55 +0330

    feat: add http handler

---
+++ go-kit-aggregator-service/aggsvc/aggtransport/http.go
@@ -0,0 +1,61 @@
+package aggtransport
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/go-kit/kit/endpoint"
+	"github.com/go-kit/kit/transport"
+	httptransport "github.com/go-kit/kit/transport/http"
+	"github.com/go-kit/log"
+	"net/http"
+
+	"github.com/pdrm26/toll-calculator/go-kit-aggregator-service/aggsvc/aggendpoint"
+)
+
+func newHTTPHandler(endpoints aggendpoint.Set, logger log.Logger) http.Handler {
+	options := []httptransport.ServerOption{
+		httptransport.ServerErrorEncoder(errorEncoder),
+		httptransport.ServerErrorHandler(transport.NewLogErrorHandler(logger)),
+	}
+	m := http.NewServeMux()
+	m.Handle("/aggregate", httptransport.NewServer(
+		endpoints.AggregateEndpoint,
+		decodeHTTPAggregateRequest,
+		encodeHTTPGenericResponse,
+		options...,
+	))
+	m.Handle("/invoice", httptransport.NewServer(
+		endpoints.InvoiceEndpoint,
+		decodeHTTPInvoiceRequest,
+		encodeHTTPGenericResponse,
+		options...,
+	))
+	return m
+}
+
+func errorEncoder(_ context.Context, err error, w http.ResponseWriter) {
+	fmt.Println("ERROR ENCODER: ", err)
+}
+
+func decodeHTTPAggregateRequest(_ context.Context, r *http.Request) (interface{}, error) {
+	var req aggendpoint.AggregateRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	return req, err
+}
+
+func decodeHTTPInvoiceRequest(_ context.Context, r *http.Request) (interface{}, error) {
+	var req aggendpoint.InvoiceRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	return req, err
+}
+
+func encodeHTTPGenericResponse(ctx context.Context, w http.ResponseWriter, response interface{}) error {
+	if f, ok := response.(endpoint.Failer); ok && f.Failed() != nil {
+		errorEncoder(ctx, f.Failed(), w)
+		return nil
+	}
+
+	w.Header().Set("Content-Type", "application/json; charset=utf-8")
+	return json.NewEncoder(w).Encode(response)
+}
